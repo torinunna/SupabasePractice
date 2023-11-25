3fca1c15cccce01e78bede9284519ebaf4c962d3
torinunna <torinunna9588@gmail.com>
2023-11-25 23:54:19 +0900

    Initial Commit

---
+++ SupabasePractice/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SupabasePractice
+//
+//  Created by YUJIN KWON on 2023/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SupabasePractice/SupabasePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupabasePracticeApp.swift
+//  SupabasePractice
+//
+//  Created by YUJIN KWON on 2023/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SupabasePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
